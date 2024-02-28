9ab3509ca6a6449978654e5fcd64bb08935070d1
xiaojianjun <jianjun.xiao@foxmail.com>
2024-02-28 12:11:50 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       }}
     >
       <label style={{ marginBottom: "20px", fontSize: "17px" }}>
-        前端候选人肖建军 非在职期个人进修内容
+        前端候选人肖建军 非在职期进修内容 建议用PC浏览
       </label>
       <a href="https://docs.qq.com/mind/DT29kWVVJenRQVGlJ">Code Complete</a>{" "}
       <br></br>
